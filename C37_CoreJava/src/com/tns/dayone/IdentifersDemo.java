e4bdcb3adc25e7f2ebb2bd48b58575346cbd4dcf
abdul <abdul@LAPTOP-E5TBMNAS>
2024-04-05 12:39:58 +0530

    Day one

--- C37_CoreJava/src/com/tns/dayone/IdentifersDemo.java
+++ C37_CoreJava/src/com/tns/dayone/IdentifersDemo.java
@@ -2,9 +2,9 @@ package com.tns.dayone;
 
 public class IdentifersDemo {
 	public static void main(String[] args) {
-		int $number = 10; //starts with $
+		int $number = 10; 
 		System.out.println("value of the number variable is : "+$number);
-		String studentName="Abdul"; //use camelcase
+		String studentName="Abdul"; 
 		System.out.println("value is : "+ studentName);
 		}
 }
